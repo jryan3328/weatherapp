6e4d70d6ef29bc1d8428ed5d918c3ca7b78a1810
Jonathan Ryan <jryan3328@gmail.com>
2021-03-13 02:50:12 -0500

    changed background images

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
     let month = months[d.getMonth()];
     let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`
+    return `${day} ${date}, ${month} of ${year}`
   }
 
   return (
